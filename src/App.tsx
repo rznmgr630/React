836f2fb4c806cc21002fc5c510dd18bf7ee4452d
rznmgr630 <rajan.midun1@gmail.com>
2024-08-01 17:26:25 +0545

    pizza app with multiple components

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header"
+import Menu from "./components/Menu";
+import './index.css';
+
+function App() {
+
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="header" >
+      <h1>Fast React Pizza CO.</h1>
+    </div>
+  )
+}
+
+export default Header;
