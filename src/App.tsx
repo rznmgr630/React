304aeb29ddc63518676fbdbc23de39912a9d3d32
rznmgr630 <rajan.midun1@gmail.com>
2024-08-03 07:50:24 +0545

    get date from today

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,35 @@ import { useState } from 'react';
 import './index.css';
 
 function App() {
-  const [currentStep, setCurrentStep] = useState<number>(1);
+  const [step, setStep] = useState<number>(1);
+  const [count, setCount] = useState<number>(0);
+
+  const getDateInfo = () => {
+    const date = new Date();
+    if (count === 0)
+      return 'Today is ' + date.toDateString()
+    else if (count < 0)
+      return count + ' days ago was ' + new Date(date.setDate(date.getDate() + count)).toDateString()
+    else
+      return count + ' days from today is ' + new Date(date.setDate(date.getDate() + count)).toDateString()
+  }
 
   return (
     <div className='container'>
-      <div className='steps'>
-        <div className={`step-item ${currentStep >= 1 && 'active'}`}>1</div>
-        <div className={`step-item ${currentStep >= 2 && 'active'}`}>2</div>
-        <div className={`step-item ${currentStep >= 3 && 'active'}`}>3</div>
+      <div>
+        <button onClick={() => setStep(prev => prev - 1)}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={() => setStep(prev => prev + 1)}>+</button>
       </div>
 
-      <div className='information'>
-        {
-          currentStep === 1 ? (
-            <div>Learn React JS</div>
-          ) : currentStep === 2 ? (
-            <div>Build some cool projects.</div>
-          )
-            : (
-              <div>Earn great money.</div>
-            )
-        }
+      <div>
+        <button onClick={() => setCount(prev => prev - step)}>-</button>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount(prev => prev + step)}>+</button>
       </div>
 
-      <div className='buttons'>
-        <button
-          disabled={currentStep === 1}
-          onClick={() => setCurrentStep(prev => prev - 1)}
-        >
-          Previous
-        </button>
-
-        <button
-          disabled={currentStep === 3}
-          onClick={() => setCurrentStep(prev => prev + 1)}
-        >
-          Next
-        </button>
+      <div>
+        <p>{getDateInfo()} </p>
       </div>
     </div>
   )
